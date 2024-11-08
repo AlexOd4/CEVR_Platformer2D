b8ee9abe356aac37476ad20a6f87a78300530427
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2024-11-08 13:00:36 +0100

    a

--- Assets/Scripts/Enemies/Crawler/EnemyFloorTrigger.cs
+++ Assets/Scripts/Enemies/Crawler/EnemyFloorTrigger.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class EnemyFloorTrigger : MonoBehaviour
 {
-    private bool _isTriggered;
 
     [SerializeField] private EnemyCrawlerMovement enemyCrawler;
+    [SerializeField] private bool isSide;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isSide)
+        enemyCrawler.CrawlingDetector();
+
+    }
 
     /// <summary>
     /// Returns for an instance true of a trigger of a collision trigger
     /// </summary>
     private void OnTriggerExit2D(Collider2D collision)
     {
-
+        if (!isSide)
         enemyCrawler.CrawlingDetector();
 
     }
