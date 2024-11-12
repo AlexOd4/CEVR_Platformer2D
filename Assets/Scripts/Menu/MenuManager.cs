2ee617113ba059f236accd5ecdfc2ca36239d79f
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2024-11-12 21:48:40 +0100

    moreMenu

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -9,6 +9,11 @@ public class MenuManager : MonoBehaviour
     [SerializeField] GameObject[] menuPanels;
     [SerializeField] Animator anim;
 
+    private void Awake()
+    {
+        //LocalLevelSelection = GameManager.LevelSelection;
+    }
+
     public void SwitchPanel(GameObject switchPanel)
     {
         foreach (GameObject panel in menuPanels)
@@ -21,9 +26,23 @@ public class MenuManager : MonoBehaviour
         GameManager.Instance.FindChildByTag(switchPanel.transform, "SelectedButton").gameObject.GetComponent<Button>();
     }
 
-    public void ChangeSceneWithAnimation(GameManager.LevelSelection levelSelected)
+    public void ChangeSceneWithAnimation(String level)
     {
-        anim.Play("fadeIn");
+        GameManager.LevelSelection levelSelected = GameManager.LevelSelection.None; 
+        if(level == "level01")
+        {
+            levelSelected = GameManager.LevelSelection.Level01;
+        }
+        else if (level == "level02")
+        {
+            levelSelected = GameManager.LevelSelection.Level02;
+        }
+        else if (level == "level03")
+        {
+            levelSelected = GameManager.LevelSelection.Level03;
+        }
+
+        anim.Play("ChangeScene_FadeIn");
         GameManager.Instance.currentLevel = levelSelected;
     }
 
