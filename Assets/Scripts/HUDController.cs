ad9de32e2f1d192cc9e6a5f2e6b54e8e2156343f
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2024-11-14 21:28:18 +0100

    Memio

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -2,17 +2,34 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class HUDController : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] Slider heartSlider;
     [SerializeField] TMP_Text scoreText;
+
+    [Header("HUDs")]
+    [SerializeField] GameObject deathHud;
+    [SerializeField] GameObject playerHud;
     private void Update()
     {
         heartSlider.value = player.GetComponent<HealthSystem>().Life;
 
         scoreText.text = "SCORE: " + GameManager.Instance.currentScore.ToString("D5");
 
+        if (!(player.GetComponent<PlayerMovementController>().enabled && player.GetComponent<PlayerInputHandler>().enabled))
+        {
+            playerHud.SetActive(false);
+            deathHud.SetActive(true);
+        }
+
     }
+
+    public void toMain()
+    {
+        GameManager.Instance.Save();
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
